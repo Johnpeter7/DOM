f372247cc6950cd67c7761c282a0d8e8d1cd6890
Johnpeter7 <93126712+Johnpeter7@users.noreply.github.com>
2023-12-16 22:53:47 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function foo(){
+   
+  var firstName = document.getElementById("firstName").value;
+  var middleName = document.getElementById("middleName").value;
+  var lastName = document.getElementById("lastName").value;
+  var email = document.getElementById("email").value;
+
+  console.log("First Name: " + firstName);
+  console.log("Middle Name: " + middleName);
+  console.log("Last Name: " + lastName);
+  console.log("Email: " + email);
+  
+};
